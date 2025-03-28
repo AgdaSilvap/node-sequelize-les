11a8be4ed764f117f8db2af7846d4377bd758a36
Sofia <sofiasartrio@gmail.com>
2025-03-28 08:46:28 -0300

    [feat] criando classes reserva e sala

---
+++ sal-node-sequelize/src/models/Sala.js
@@ -0,0 +1,40 @@
+import { Model, DataTypes } from 'sequelize';
+
+export default class Autor extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: 'O nome deve ser preenchido!' },
+          len: { args: [3, 50], msg: 'O nome deve ter entre 3 e 50 caracteres!' },
+        }
+      },
+      nascimento: {
+        type: DataTypes.DATE,
+        validate: {
+          notEmpty: { msg: 'A data de nascimento deve ser preenchida!' },
+        }
+      },
+      dtCarga: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      nacionalidade: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: 'A nacionalidade deve ser preenchida!' },
+          len: { args: [5, 30], msg: 'A nacionalidade deve ter entre 3 e 30 caracteres!' },
+        }
+      }
+    }, {
+      sequelize, modelName: 'autor', tableName: 'autores'
+    })
+  }
+
+  static associate(models) {
+    this.hasMany(models.livro, { as: 'livros', foreignKey: 'id_autor', allowNull: false, onDelete: 'CASCADE' });
+  }
+}
+
+export { Autor }
