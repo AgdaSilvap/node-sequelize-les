91ee70b5bbfc68765e217a3321bb5b2bf8f008f5
Agda Silva <agdasilvap@gmail.com>
2025-04-02 19:04:25 -0300

    fix: adição de dois inserts solicitados

--- sal-node-sequelize/src/config/database-connection.js
+++ sal-node-sequelize/src/config/database-connection.js
@@ -36,6 +36,18 @@ function databaseInserts() {
       nacionalidade: 'Inglesa'
     });
 
+    const autor3 = await Autor.create({
+      nome: 'Jhon Grey',
+      nascimento: '1977-08-29',
+      nacionalidade: 'Estadunidense'
+    });
+
+    const autor4 = await Autor.create({
+      "nome": "Gabriel García Márquez",
+      "nascimento": "1927-03-06",
+      "nacionalidade": "Colombiano"
+    });
+
     const editora1 = await Editora.create({
       nome: 'Rocco',
       cnpj: '00.000.000/0001-00',
@@ -48,6 +60,20 @@ function databaseInserts() {
       cnpj: '00.000.000/0002-00',
       edereco: 'Rua 2, 2',
       telefone: '222222-2222'
+    });
+
+    const editora3 = await Editora.create({
+      "nome": "Editora Globo",
+      "cnpj": "11.111.111/0001-11",
+      "endereco": "Avenida Paulista, 1000",
+      "telefone": "3333-3333"
+    });
+
+    const editora4 = await Editora.create({
+      "nome": "Companhia das Letras",
+      "cnpj": "22.222.222/0002-22",
+      "endereco": "Rua dos Editores, 500",
+      "telefone": "4444-4444"
     });
 
     const feedback1 = await Feedback.create({
@@ -68,57 +94,23 @@ function databaseInserts() {
       dtCarga: new Date()
     });
 
-    //para testes COM erros
+    const feedback3 = await Feedback.create({
+      experiencia: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      avaliacao: true,
+      quantidadePessoas: 1,
+      id_cliente: 1,
+      id_reserva_sala: 1,
+      dtCarga: new Date()
+    });
 
-    //SEM DATA DE NASCIMENTO - ATRIBUTO NÃO PODE SER NULL
-    // const autor1 = await Autor.create({
-    //   nome: 'J.K. Rowling',
-    //   nascimento: '',
-    //   nacionalidade: 'Inglesa'
-    // });
-
-    // TAMANHO DA NACIONALIDADE MENOR QUE O PERMITIDO
-    // const autor2 = await Autor.create({
-    //   nome: 'J.R.R. Tolkien',
-    //   nascimento: '1892-01-03',
-    //   nacionalidade: 'En'
-    // });
-
-    //SEM ENDEREÇO - ATRIBUTO NÃO PODE SER NULL
-    // const editora1 = await Editora.create({
-    //   nome: 'Rocco',
-    //   cnpj: '00.000.000/0001-00',
-    //   edereco: '',
-    //   telefone: '(11) 1111-1111'
-    // });
-
-    // // TAMANHO DO CNPJ MAIOR QUE O PERMITIDO
-    // const editora2 = await Editora.create({
-    //   nome: 'Alta Books',
-    //   cnpj: '00.000.000/ 0002-00',
-    //   edereco: 'Rua 2, 2',
-    //   telefone: '222222-2222'
-    // });
-
-    //NÚMERO MÍNIMO DE CARACTERES DA EXPERIÊNCIA NÃO ATENDIDO
-    // const feedback1 = await Feedback.create({
-    //   experiencia: 'OK',
-    //   avaliacao: true,
-    //   quantidadePessoas: 1,
-    //   id_cliente: 1,
-    //   id_reserva_sala: 1,
-    //   dtCarga: new Date()
-    // });
-
-    //QUANTIDADE DE PESSOAS NÃO INFORMADA
-    // const feedback2 = await Feedback.create({
-    //   experiencia: 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-    //   avaliacao: true,
-    //   quantidadePessoas: null,
-    //   id_cliente: 2,
-    //   id_reserva_sala: 2,
-    //   dtCarga: new Date()
-    // });
+    const feedback4 = await Feedback.create({
+      experiencia: 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+      avaliacao: true,
+      quantidadePessoas: 5,
+      id_cliente: 2,
+      id_reserva_sala: 2,
+      dtCarga: new Date()
+    });
 
 
   })()
