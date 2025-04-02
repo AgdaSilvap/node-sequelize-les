8dbab0051489b79bf12a1d6862057f576290e3a8
Agda Silva <agdasilvap@gmail.com>
2025-04-02 19:51:51 -0300

    fix: correção na ordem dos inserts

--- sal-node-sequelize/src/config/database-connection.js
+++ sal-node-sequelize/src/config/database-connection.js
@@ -40,100 +40,8 @@ function databaseInserts() {
 
     await sequelize.sync({ force: true })
 
-    //aluna : AGDA
-    //inserindo 4 autores 
-    const autor1 = await Autor.create({
-      nome: 'J.K. Rowling',
-      nascimento: '1965-07-31',
-      nacionalidade: 'Inglesa'
-    });
 
-    const autor2 = await Autor.create({
-      nome: 'J.R.R. Tolkien',
-      nascimento: '1892-01-03',
-      nacionalidade: 'Inglesa'
-    });
-
-    const autor3 = await Autor.create({
-      nome: 'Jhon Grey',
-      nascimento: '1977-08-29',
-      nacionalidade: 'Estadunidense'
-    });
-
-    const autor4 = await Autor.create({
-      "nome": "Gabriel García Márquez",
-      "nascimento": "1927-03-06",
-      "nacionalidade": "Colombiano"
-    });
-
-    //inserindo 4 editoras
-    const editora1 = await Editora.create({
-      nome: 'Rocco',
-      cnpj: '00.000.000/0001-00',
-      edereco: 'Rua 1, 1',
-      telefone: '111111-1111'
-    });
-
-    const editora2 = await Editora.create({
-      nome: 'Alta Books',
-      cnpj: '00.000.000/0002-00',
-      edereco: 'Rua 2, 2',
-      telefone: '222222-2222'
-    });
-
-    const editora3 = await Editora.create({
-      "nome": "Editora Globo",
-      "cnpj": "11.111.111/0001-11",
-      "endereco": "Avenida Paulista, 1000",
-      "telefone": "3333-3333"
-    });
-
-    const editora4 = await Editora.create({
-      "nome": "Companhia das Letras",
-      "cnpj": "22.222.222/0002-22",
-      "endereco": "Rua dos Editores, 500",
-      "telefone": "4444-4444"
-    });
-
-    //inserindo 4 feedbacks
-    const feedback1 = await Feedback.create({
-      experiencia: 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-      avaliacao: true,
-      quantidadePessoas: 1,
-      id_cliente: 1,
-      id_reserva_sala: 1,
-      dtCarga: new Date()
-    });
-
-    const feedback2 = await Feedback.create({
-      experiencia: 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-      avaliacao: true,
-      quantidadePessoas: 1,
-      id_cliente: 2,
-      id_reserva_sala: 2,
-      dtCarga: new Date()
-    });
-
-    const feedback3 = await Feedback.create({
-      experiencia: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      avaliacao: true,
-      quantidadePessoas: 1,
-      id_cliente: 1,
-      id_reserva_sala: 1,
-      dtCarga: new Date()
-    });
-
-    const feedback4 = await Feedback.create({
-      experiencia: 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
-      avaliacao: true,
-      quantidadePessoas: 5,
-      id_cliente: 2,
-      id_reserva_sala: 2,
-      dtCarga: new Date()
-    });
-
-    //aluna: BIANCA
-    // Inserindo 4 clientes
+    // Inserindo 4 clientes - BIANCA
     const cliente1 = await Cliente.create({
       dsNome: 'Ana Silva',
       dsCpf: '123.456.789-01',
@@ -211,43 +119,60 @@ function databaseInserts() {
       status: true
     });
 
-    // Inserindo 4 aluguéis de livro
-    const aluguel1 = await AluguelDeLivro.create({
-      dtAluguel: new Date('2025-03-01'),
-      dtDevolucao: new Date('2025-03-15'),
-      dsTipoAluguel: 'Mensal',
-      vlTotal: 50.0,
-      clienteId: cliente1.id,
-      funcionarioId: funcionario1.id
+    //aluna : AGDA
+    //inserindo 4 autores 
+    const autor1 = await Autor.create({
+      nome: 'J.K. Rowling',
+      nascimento: '1965-07-31',
+      nacionalidade: 'Inglesa'
     });
 
-    const aluguel2 = await AluguelDeLivro.create({
-      dtAluguel: new Date('2025-03-05'),
-      dtDevolucao: new Date('2025-03-20'),
-      dsTipoAluguel: 'Quinzenal',
-      vlTotal: 30.0,
-      clienteId: cliente2.id,
-      funcionarioId: funcionario2.id
+    const autor2 = await Autor.create({
+      nome: 'J.R.R. Tolkien',
+      nascimento: '1892-01-03',
+      nacionalidade: 'Inglesa'
     });
 
-    const aluguel3 = await AluguelDeLivro.create({
-      dtAluguel: new Date('2025-03-10'),
-      dtDevolucao: new Date('2025-03-25'),
-      dsTipoAluguel: 'Semanal',
-      vlTotal: 20.0,
-      clienteId: cliente3.id,
-      funcionarioId: funcionario3.id
+    const autor3 = await Autor.create({
+      nome: 'Jhon Grey',
+      nascimento: '1977-08-29',
+      nacionalidade: 'Estadunidense'
     });
 
-    const aluguel4 = await AluguelDeLivro.create({
-      dtAluguel: new Date('2025-03-15'),
-      dtDevolucao: new Date('2025-03-30'),
-      dsTipoAluguel: 'Mensal',
-      vlTotal: 50.0,
-      clienteId: cliente4.id,
-      FuncionarioId: funcionario4.id
+    const autor4 = await Autor.create({
+      "nome": "Gabriel García Márquez",
+      "nascimento": "1927-03-06",
+      "nacionalidade": "Colombiano"
     });
 
+    //inserindo 4 editoras
+    const editora1 = await Editora.create({
+      nome: 'Rocco',
+      cnpj: '00.000.000/0001-00',
+      edereco: 'Rua 1, 1',
+      telefone: '111111-1111'
+    });
+
+    const editora2 = await Editora.create({
+      nome: 'Alta Books',
+      cnpj: '00.000.000/0002-00',
+      edereco: 'Rua 2, 2',
+      telefone: '222222-2222'
+    });
+
+    const editora3 = await Editora.create({
+      "nome": "Editora Globo",
+      "cnpj": "11.111.111/0001-11",
+      "endereco": "Avenida Paulista, 1000",
+      "telefone": "(22) 3333-3333"
+    });
+
+    const editora4 = await Editora.create({
+      "nome": "Companhia das Letras",
+      "cnpj": "22.222.222/0002-22",
+      "endereco": "Rua dos Editores, 500",
+      "telefone": "(11) 4444-4444"
+    });
     //aluna: SOFIA
 
     //inserindo livros
@@ -304,6 +229,83 @@ function databaseInserts() {
       funcionarioId: 2,
       salaId: sala2.id
     });
+
+    //inserindo 4 feedbacks - AGDA
+    const feedback1 = await Feedback.create({
+      experiencia: 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+      avaliacao: true,
+      quantidadePessoas: 1,
+      id_cliente: 1,
+      id_reserva_sala: 1,
+      dtCarga: new Date()
+    });
+
+    const feedback2 = await Feedback.create({
+      experiencia: 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+      avaliacao: true,
+      quantidadePessoas: 1,
+      id_cliente: 2,
+      id_reserva_sala: 2,
+      dtCarga: new Date()
+    });
+
+    const feedback3 = await Feedback.create({
+      experiencia: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      avaliacao: true,
+      quantidadePessoas: 1,
+      id_cliente: 1,
+      id_reserva_sala: 1,
+      dtCarga: new Date()
+    });
+
+    const feedback4 = await Feedback.create({
+      experiencia: 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+      avaliacao: true,
+      quantidadePessoas: 5,
+      id_cliente: 2,
+      id_reserva_sala: 2,
+      dtCarga: new Date()
+    });
+
+
+
+    // Inserindo 4 aluguéis de livro - BIANCA
+    const aluguel1 = await AluguelDeLivro.create({
+      dtAluguel: new Date('2025-03-01'),
+      dtDevolucao: new Date('2025-03-15'),
+      dsTipoAluguel: 'Mensal',
+      vlTotal: 50.0,
+      clienteId: cliente1.id,
+      funcionarioId: funcionario1.id
+    });
+
+    const aluguel2 = await AluguelDeLivro.create({
+      dtAluguel: new Date('2025-03-05'),
+      dtDevolucao: new Date('2025-03-20'),
+      dsTipoAluguel: 'Quinzenal',
+      vlTotal: 30.0,
+      clienteId: cliente2.id,
+      funcionarioId: funcionario2.id
+    });
+
+    const aluguel3 = await AluguelDeLivro.create({
+      dtAluguel: new Date('2025-03-10'),
+      dtDevolucao: new Date('2025-03-25'),
+      dsTipoAluguel: 'Semanal',
+      vlTotal: 20.0,
+      clienteId: cliente3.id,
+      funcionarioId: funcionario3.id
+    });
+
+    const aluguel4 = await AluguelDeLivro.create({
+      dtAluguel: new Date('2025-03-15'),
+      dtDevolucao: new Date('2025-03-30'),
+      dsTipoAluguel: 'Mensal',
+      vlTotal: 50.0,
+      clienteId: cliente4.id,
+      FuncionarioId: funcionario4.id
+    });
+
 
   })()
 }
