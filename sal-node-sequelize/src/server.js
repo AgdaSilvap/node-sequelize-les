e0c57bcc504f80fd9e0234a7aedea0963c16100f
Agda Silva <agdasilvap@gmail.com>
2025-04-10 19:02:27 -0300

    fix: ajuste no server.js após mudanças

--- sal-node-sequelize/src/server.js
+++ sal-node-sequelize/src/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import './config/database-connection.js'
+import { routes } from './routes.js';
+import errorHandler from './middleware/error-handler.js';
+import { sequelize } from './config/database-connection.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
+app.use(errorHandler);
 app.listen(3333, () => {
     console.log('Server started on port 3333');
 });
