636c0b426b86124aa117850590bfd24d3e3d5e90
namikki <iambiancarodrigues@gmail.com>
2025-04-18 11:33:42 -0300

    [feat] criando o error-handler

--- sal-node-sequelize/src/server.js
+++ sal-node-sequelize/src/server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import routes from './routes.js';
 import './config/database-connection.js';
+import errorHandler from './_middleware/error-handler.js';
 
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
 app.listen(3333, () => {
     console.log('Server started on port 3333');
 });
