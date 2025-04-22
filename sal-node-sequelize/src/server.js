09234ab78813a93ebb3b568a78e089234f16dca5
Agda Silva <agdasilvap@gmail.com>
2025-04-22 20:07:37 -0300

    fix: ajuste nas importações do arquivo

--- sal-node-sequelize/src/server.js
+++ sal-node-sequelize/src/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
-
-import { routes } from './routes.js';
+import routes from './routes.js';
 import errorHandler from './middleware/error-handler.js';
-import { sequelize } from './config/database-connection.js';
+
+import sequelize from './config/database-connection.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
+
 app.listen(3333, () => {
     console.log('Server started on port 3333');
 });
