5cb531c41a4dadd2194392c66e2bc9f5b159a517
Agda Silva <agdasilvap@gmail.com>
2025-04-25 18:00:35 -0300

    fix: correção do metodo create

--- sal-node-sequelize/src/services/FeedbackService.js
+++ sal-node-sequelize/src/services/FeedbackService.js
@@ -1,4 +1,5 @@
 import { Feedback } from "../models/Feedback.js";
+import sequelize from "../config/database-connection.js";
 
 class FeedbackService {
   static async findAll() {
@@ -14,15 +15,20 @@ class FeedbackService {
 
 
   static async create(req) {
-    const { reserva, experiencia, avaliacao, quantidadePessoas } = req.body;
-    if (reserva == null) throw 'A reserva deve ser informada!';
-    if (experiencia == null || 3 > length(experiencia) > 255) throw 'a experiência deve ser descrita com no mínimo 3 e no máximo 255 caracteres!';
+    const { reservaId, experiencia, avaliacao, quantidadePessoas } = req.body;
+    if (reservaId == null) throw 'A reserva deve ser informada!';
+    if (experiencia == null || 3 > experiencia.lenght > 255) throw 'a experiência deve ser descrita com no mínimo 3 e no máximo 255 caracteres!';
     if (quantidadePessoas == null) throw 'A quantidade de pessoas deve ser informada!';
 
-    const transaction = await Sequelize.transaction();
-
-    const feedback = await Feedback.create({ reservaId: reserva.id, experiencia, avaliacao, quantidadePessoas, feedbackId: feedback.id }, { transaction: transaction });
-    return await Feedback.findByPk(feedback.id, { include: { all: true, nested: true } });
+    const transaction = await sequelize.transaction();
+    const feedback = await Feedback.create({ reservaId: reservaId, experiencia, avaliacao, quantidadePessoas }, { transaction: transaction });
+    try {
+      await transaction.commit();
+      return await Feedback.findByPk(feedback.id, { include: { all: true, nested: true } });
+    } catch (error) {
+      await transaction.rollback();
+      throw "Caiu no catch";
+    }
   }
 
   static async update(req) {
