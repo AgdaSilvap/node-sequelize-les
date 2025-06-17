4824232de52b1bd5b5adfc02433782e2f1d86b58
Agda Silva <agdasilvap@gmail.com>
2025-06-17 10:38:57 -0300

    fix: port on server.js

--- src/server.js
+++ src/server.js
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(3333, () => {
-    console.log('Server started on port 3333');
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
